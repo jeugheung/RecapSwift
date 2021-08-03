2096fca19d375cb56d426d662890ff6175ce7a2b
Andrey Kim <akart.dev@gmail.com>
2021-08-04 02:01:38 +0600

    space

--- 2. Data types.playground/Contents.swift
+++ 2. Data types.playground/Contents.swift
@@ -7,7 +7,7 @@ let y: String = "Korea"
 let z: Double = 1.00001 // 15 после запятой
 let w: Float = 1.123456 // 6 после запятой
 let d: Int = 12333
-let b: Bool = true //false
+let b: Bool = true // или false
 
 let f = Int(123.123)
 let v = Float(123.13233232)
--- 4. String Interpolation.playground/Contents.swift
+++ 4. String Interpolation.playground/Contents.swift
@@ -5,7 +5,7 @@ let weatherDescription = "Temperature is about 12 above zero and wind speed is a
 let temperature = 12
 let windSpeed = 5
 
-// Interpolation "\()"
+// Интерполяция "\()"
 let weatherDescriptionI = "Temperature is about \(temperature) above zero and wind speed is about \(windSpeed) m/s"
 
 let string = "\(1 + 2)" + "\(2 + 3)"
