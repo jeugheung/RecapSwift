5d1f5a55c211a2ca225f7d5e3cbf454cdd42f3be
Andrey Kim <akart.dev@gmail.com>
2021-08-03 23:08:01 +0600

    basic themes

---
+++ 2. Data types.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// Character, String, Int, Double, Float, Bool
+
+let x: Character = "q"
+let y: String = "Korea"
+let z: Double = 1.00001 // 15 после запятой
+let w: Float = 1.123456 // 6 после запятой
+let d: Int = 12333
+let b: Bool = true //false
+
+let f = Int(123.123)
+let v = Float(123.13233232)
