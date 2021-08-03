9a24a8aa6ab13c53f54c02aea65a557ee20c5e13
Andrey Kim <akart.dev@gmail.com>
2021-08-03 22:03:05 +0600

    first commit

---
+++ 1. Constants and Variables.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Constants
+let maximumNumberOfLoginAttempts = 10
+
+//Variables
+var currentLoginAttempt = 0
+
+//You can declare multiple constants or multiple variables on one line, separating them with commas
+var x = 0.0, y = 0.0, z = 0.0
+let m = 1.0, g = 0.1, t = 1.3
+
